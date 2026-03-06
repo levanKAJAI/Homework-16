c58c9ea193c55be4f507f49d413b6f97557dd492
levan kajaia <133898764+levanKAJAI@users.noreply.github.com>
2026-03-06 14:28:56 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,25 @@
-function compareWords(word1, word2) {
+function convertTime() {
 
-    if (word1.length === word2.length) {
-        return true;
-    } else {
-        return false;
+    let time = prompt("შეიყვანე დრო (მაგალითად: 10:23)");
+    let period = prompt("შეიყვანეთ PM ან AM").toUpperCase();
+
+    let parts = time.split(":");
+    let hours = Number(parts[0]);
+    let minutes = parts[1];
+
+    if (period === "PM" && hours < 12) {
+        hours += 12;
     }
 
+    if (period === "AM" && hours === 12) {
+        hours = 0;
+    }
+
+    let result = hours.toString().padStart(2, "0") + ":" + minutes;
+
+    alert("მიმდინარე დროა: " + result);
+    console.log("მიმდინარე დროა: " + result);
 }
 
-let word1 = prompt("შეიყვანეთ პირველი სიტვყა:");
-let word2 = prompt("შეიყვანეთ მეორე სიტყვა:");
+convertTime();
 
-console.log(compareWords(word1, word2));
